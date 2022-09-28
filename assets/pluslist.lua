43d276be190b49bfd647c2c4170144ce97c6e459
niqu1337 <thebogat@wp.pl>
2022-09-28 19:32:06 +0200

    update

--- assets/pluslist.lua
+++ assets/pluslist.lua
@@ -2,5 +2,6 @@ return {
     325960461, -- niqudev
     3802675978, -- vertical1337
     662905266, -- kruszek31
-    280539180 -- TeresaGriffith8 (aka mort)
+    280539180, -- TeresaGriffith8 (aka mort)
+    32755093 -- adamdud
 }
