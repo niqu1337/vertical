c1130ea028b942e4c5cd90e71d9e15068c4f7c66
niqu1337 <niquus.business@gmail.com>
2022-11-29 00:38:05 +0100

    update

--- assets/db/pluslist.lua
+++ assets/db/pluslist.lua
@@ -4,9 +4,11 @@ return {
 	2624931481, -- Blitz13150223
 	2736525407, -- ovecka45239
 	652369564, -- v576_3
-	1741485961, -- xL00king (Beta Tester)
+	1741485961, -- xL00king
+	581023521, -- wv_te
 	454187280, -- spIooq (Booster)
 	3797929273, -- ulkucudhc (Booster)
 	135445381, -- SuzulIz (Booster)
 	2633688242, -- salvador76546OG (Booster)
+	3164974566 -- lIpewIlpewIl (Booster)
 }
