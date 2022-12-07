849f64f7b6735e82476ea3a5f07c18bf4cf961c9
niqu1337 <niquus.business@gmail.com>
2022-12-07 17:31:32 +0100

    update

--- assets/espmodule.lua
+++ assets/espmodule.lua
@@ -496,7 +496,7 @@ function espLibrary:Load(renderValue)
                     objects.bottom.Font = self.options.font;
                     objects.bottom.Size = self.options.fontSize;
                     objects.bottom.Transparency = self.options.distanceTransparency;
-                    objects.bottom.Color = color or self.options.nameColor;
+                    objects.bottom.Color = color or self.options.distanceColor;
                     objects.bottom.Text = tostring(round(distance)) .. self.options.distanceSuffix;
                     objects.bottom.Position = round(position + vector2New(size.X * 0.5, size.Y + 1));
     
